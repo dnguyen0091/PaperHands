427099413b16f0675892efe0e377384302cbfdbf
dnguyen0091 <davidnguyen0091@icloud.com>
2025-12-30 08:41:38 -0500

    added page to routes

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,7 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 // import { AuthProvider } from '../context/AuthContext.tsx'
 import './App.css'
 import LandingPage from './Landing Page/landingPage.tsx'
+import ErrorPage from "./Page Down/error.tsx"
 import UnderConstruction from "./Page Down/underConstruction.tsx"
+import UnderMaintenance from "./Page Down/underMaintenance.tsx"
 function App() {
 
   return (
@@ -11,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage/>}/>
           <Route path="/under-construction" element={<UnderConstruction/>}/>
+          <Route path="/error" element={<ErrorPage/>}/>
+          <Route path="/under-maintenance" element={<UnderMaintenance/>}/>
         </Routes>
       </BrowserRouter>
     // </AuthProvider>
