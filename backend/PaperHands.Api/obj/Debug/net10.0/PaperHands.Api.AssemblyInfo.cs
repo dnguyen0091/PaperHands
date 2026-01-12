7df24485a04c0e888a68825b290abfa24e079900
dnguyen0091 <davidnguyen0091@icloud.com>
2026-01-12 14:38:38 -0500

    fix: incorrect file types

--- backend/PaperHands.Api/obj/Debug/net10.0/PaperHands.Api.AssemblyInfo.cs
+++ backend/PaperHands.Api/obj/Debug/net10.0/PaperHands.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaperHands.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4090722ad5c20a5d18d8f82141c8b3fe81c2f06d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64324d50ae3e54355009a411b4678795fa052caa")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaperHands.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaperHands.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
